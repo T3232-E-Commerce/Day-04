a3b9e816079a255a5248f0368f301141e9f36b3b
DilmyPerera <samadiniperera20@gmail.com>
2025-04-13 14:00:36 +0530

    Update CRUDcontroller.java

--- myapp/src/main/java/lk/ac/vau/fas/controller/CRUDcontroller.java
+++ myapp/src/main/java/lk/ac/vau/fas/controller/CRUDcontroller.java
@@ -11,50 +11,42 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class CRUDcontroller<K,T> {
-	private Map<K,T> objects = new HashMap<K,T>();
-	
-	public Map<K,T>getmap() {
-		return objects;
-	}
-	
-	//get all
-	@GetMapping("/")
-	public Map<K,T> getAll(){
-		return objects;
-	}
-	
-	//get by id
-	@GetMapping("/{id}")
-	public T get(@PathVariable("id") K id) {
-		return objects.get(id);
-	}
-	
-	//add
-	@PostMapping("/")
-	public T add(@RequestBody T object, @RequestBody K id) {
-	    objects.put(id, object);
-	    return object;
-	}
-	
-	//update
-	@PutMapping("/{id}")
-	public T update(@PathVariable("id") K id, @RequestBody T object) {
-	    if (objects.containsKey(id)) {
-	        objects.put(id, object);
-	        return object;
-	    }
-	    return null; 
-	}
-
-	//delete
-	@DeleteMapping("/{id}")
-	public boolean delete(@PathVariable("id") K id) {
-	    if (objects.containsKey(id)) {
-	        objects.remove(id);
-	        return true;
-	    }
-	    return false; 
-	}
+    private Map<K,T> objects = new HashMap<K,T>();
+    
+    public Map<K,T> getmap() {
+        return objects;
+    }
+    
+    @GetMapping("/")
+    public Map<K,T> getAll(){
+        return objects;
+    }
+    
+    @GetMapping("/{id}")
+    public T get(@PathVariable("id") K id) {
+        return objects.get(id);
+    }
+    
+    @PostMapping("/")
+    public T add(@RequestBody T object) {
+        // This method needs to be implemented in subclasses
+        // since we need type-specific way to get the ID
+        return null;
+    }
+    
+    @PutMapping("/{id}")
+    public T update(@PathVariable("id") K id, @RequestBody T object) {
+        // This method needs to be implemented in subclasses
+        // since we need type-specific way to handle updates
+        return null;
+    }
+    
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable("id") K id) {
+        if (objects.containsKey(id)) {
+            objects.remove(id);
+            return true;
+        }
+        return false;
+    }
 }
-
-
